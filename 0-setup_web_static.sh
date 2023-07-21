b9bc56bfa7ed2d1944f0a9d824fd0b9ab8cd838c
Pascal <persiewailer697@gmail.com>
2023-07-21 07:57:29 -0700

    Compress before sending

--- 0-setup_web_static.sh
+++ 0-setup_web_static.sh
@@ -1,70 +1,25 @@
 #!/usr/bin/env bash
-# Sets up a web server for deployment of web_static.
-
-# Check if running with root privileges
-if [[ $EUID -ne 0 ]]; then
-    echo "This script must be run with root privileges." >&2
-    exit 1
-fi
 
 # Install Nginx if not already installed
-if ! command -v nginx &>/dev/null; then
-    echo "Installing Nginx..."
-    apt-get update
-    apt-get -y install nginx
-fi
+sudo apt-get update
+sudo apt-get install -y nginx
 
-web_static_dir="/data/web_static"
-current_link="$web_static_dir/current"
+# Create necessary folders if they don't exist
+sudo mkdir -p /data/web_static/releases/test
+sudo mkdir -p /data/web_static/shared
 
-# Create necessary directories if they don't exist
-mkdir -p "$web_static_dir/releases/test"
-mkdir -p "$web_static_dir/shared"
+# Create a fake HTML file for testing
+echo -e "<html>\n  <head>\n  </head>\n  <body>\n    Holberton School\n  </body>\n</html>" | sudo tee /data/web_static/releases/test/index.html
 
-# Create a test HTML file
-echo "<html>
-    <head>
-    </head>
-    <body>
-        <h1>Holberton School</h1>
-    </body>
-</html>" > "$web_static_dir/releases/test/index.html"
+# Create or update symbolic link
+sudo ln -sf /data/web_static/releases/test /data/web_static/current
 
-# Create a symbolic link
-if [ -L "$current_link" ]; then
-    rm "$current_link"
-fi
-ln -s "$web_static_dir/releases/test" "$current_link"
+# Give ownership of the /data/ folder to the ubuntu user and group
+sudo chown -R ubuntu:ubuntu /data/
 
-# Set ownership of the /data/ folder recursively
-chown -R ubuntu:ubuntu "$web_static_dir"
+# Update Nginx configuration to serve the content of /data/web_static/current/
+# under the alias /hbnb_static
+sudo sed -i '/listen 80 default_server;/a \\n\tlocation /hbnb_static {\n\t\talias /data/web_static/current/;\n\t}\n' /etc/nginx/sites-available/default
 
-# Update Nginx configuration
-config_file="/etc/nginx/sites-available/default"
-nginx_config="server {
-    listen 80 default_server;
-    listen [::]:80 default_server;
-    add_header X-Served-By \$hostname;
-    root   /var/www/html;
-    index  index.html index.htm;
-    location /hbnb_static {
-        alias $web_static_dir/current;
-        index index.html index.htm;
-    }
-    location /redirect_me {
-        return 301 http://github.com/Persie-O;
-    }
-    error_page 404 /404.html;
-    location /404 {
-        root /var/www/html;
-        internal;
-    }
-}"
-
-if ! grep -q "location /hbnb_static" "$config_file"; then
-    printf "%s" "$nginx_config" > "$config_file"
-    nginx -t
-    service nginx restart
-fi
-
-exit 0
+# Restart Nginx to apply the changes
+sudo service nginx restart
